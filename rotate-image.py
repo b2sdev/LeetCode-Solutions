c92c28e39bd49c7be4095f139eabeaac5f461a31
golbanghacker <snowman.won@gmail.com>
2024-06-19 20:51:19 +0900

    Add functional style solutions

--- rotate-image.py
+++ rotate-image.py
@@ -3,30 +3,34 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix):
+        matrix[:] = list(map(list, zip(*matrix[::-1])))
+        return matrix
+
+    def rotate0(self, matrix):
         H, W = len(matrix), len(matrix[0])
 
         # Transpose the matrix
         for r in range(H):
-            for c in range(r, W):
+            for c in range(r, H):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
 
         # Reverse each row
-        for r in range(H):
+        for r in range(W):
             matrix[r] = matrix[r][::-1]
 
         return matrix
 
+
 s = Solution()
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 assert s.rotate(matrix) == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12]
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+assert s.rotate(matrix) == [
+    [15, 13, 2, 5],
+    [14, 3, 4, 1],
+    [12, 6, 8, 9],
+    [16, 7, 10, 11],
 ]
-print(s.rotate(matrix))
+# matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+# print(s.rotate0(matrix))
 # Output: [[9, 5, 1], [10, 6, 2], [11, 7, 3], [12, 8, 4]]
--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,12 +1,18 @@
 from collections import deque
 
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        s = "".join(filter(str.isalnum, s)).lower()
+        return s == s[::-1]
+
+    def isPalindrome0(self, s: str) -> bool:
         deq = deque([ch for ch in s.lower() if ch.isalnum()])
         while len(deq) > 1:
             if deq.popleft() != deq.pop():
                 return False
         return True
+
 
 s = Solution()
 assert s.isPalindrome("A man, a plan, a canal: Panama") == True
