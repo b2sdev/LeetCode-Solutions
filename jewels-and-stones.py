cb9f8be9df13a2cda1096f82912b1147efc6e66f
golbanghacker <snowman.won@gmail.com>
2024-06-19 21:02:57 +0900

    Add functional style solutions

--- jewels-and-stones.py
+++ jewels-and-stones.py
@@ -1,6 +1,10 @@
 from collections import Counter
 
+
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum(1 for stone in stones if stone in set(jewels))
+
+    def numJewelsInStones0(self, jewels: str, stones: str) -> int:
         freq = Counter(stones)
         return sum(freq[jewel] for jewel in jewels)
--- two-sum.py
+++ two-sum.py
@@ -1,7 +1,16 @@
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return next(
+            [i, j + (i + 1)]
+            for i, x in enumerate(nums)
+            for j, y in enumerate(nums[i + 1 :])
+            if x + y == target
+        )
+
+    def twoSum0(self, nums: List[int], target: int) -> List[int]:
         num_dict = {}
         for i, num in enumerate(nums):
             complement = target - num
@@ -10,7 +19,8 @@ class Solution:
             num_dict[num] = i
         return None
 
+
 s = Solution()
-assert s.twoSum([2,7,11,15], 9) == [0,1]
-assert s.twoSum([3,2,4], 6) == [1,2]
-assert s.twoSum([3,3], 6) == [0,1]
+assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
+assert s.twoSum([3, 2, 4], 6) == [1, 2]
+assert s.twoSum([3, 3], 6) == [0, 1]
